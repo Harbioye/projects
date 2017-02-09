14caa4c9b6de31c784a1897311964111ce4170f2
Harbioye <marwatmohammed@yahoo.com>
2017-02-08 20:28:22 -0500

    Update shell2.c

--- shell2.c
+++ shell2.c
@@ -4,19 +4,17 @@
 #include <string.h>
 #include <unistd.h>     
 
-//functions declaration
-
-//char *strtok(char *str, const char *delim);
-
 /*EXTERN is used to declare a global variable or function in another file.
 */   
 extern char **environ;
 
-   //function definitions
+// -- FUNCTION PROTOTYPES --
 void clear_screen();
 void list_directory();
 void list_environment();
 void quit_program();
+// -- ENDS --
+
 
 /*clr -- clear the screen using the system function clear --> system("clear")
 */
@@ -72,15 +70,17 @@ int main()
    char* breakline;
    char *tokenline[maxLine];
    int n;     
-   
-   //char myStringCopy[maxLine];
+   int m;
    
    //actual initialization
    n = 0;
-   
-   //array of functions
-   //const char *funArray[] = {clear_screen(), list_directory(),list_environment(),quit_program()};
-   
+
+ /*notice the prototype
+   arrays points at the respective functions
+  */
+   void (*functionArray[4])() = {&clear_screen, &list_directory, &list_environment, &quit_program};
+
+
    /*make a list of all aliases/internal controls -- declare and initailize array
      A string array is an array of characters and the use of const char tells 
      the compiler that there is no intention to change the data in aliases.
@@ -94,7 +94,6 @@ int main()
    {      
       printf("Enter a command line argument\n");
       
-      // getline retrieves user imput via stdin then stores itin myString.
       getline (&myString, &maxLine, stdin);
             
       /*tokenize keyboard input/line using strtok using a for loop
@@ -105,40 +104,26 @@ int main()
          tokenline[n] = malloc(strlen(breakline)+1);        //allocate some space for new array
          
          strcpy(tokenline[n],breakline);
-         
-         //printf("tokenline[%d] = %s\n",n, tokenline[n]);
-      
+               
       /*Next is the comparism of the arrays aliases and tokenline 
             by calling the functions above
-       */  
-         if(strcmp(tokenline[0], aliases[0]) == 0)
-         {
-            clear_screen();
-         }
-         else if(strcmp(tokenline[0], aliases[1]) == 0)
-         {  
-            list_directory();     
-         }
-         else if(strcmp(tokenline[0], aliases[2]) == 0)
-         {
-            list_environment();
-         }
-         else if(strcmp(tokenline[0], aliases[3]) == 0)
-         {
-            quit_program();
-         }
-         
-         /* Use standard ANSI C system function to execute the line through the default system shell.
-         */
-         else
-         {
-            system(myString);
-         }
-         //n++;
-      
+       */
+         for(m = 0; m < sizeof(aliases); m++)    //loop through the array elements for comparison
+         {   
+            if(strcmp(tokenline[0], aliases[m]) == 0)
+            {
+               functionArray[m]();
+            }           
+            /* Use standard ANSI C system function to execute the line through the default system shell.
+            */
+            else
+            {
+               system(myString);
+            }
+         }         
+         /* Deallocate allocated memory */
+         free(tokenline[n]);   
       }
-   /* Deallocate allocated memory */
-      free(tokenline[n]);   
    }      
    return(0);
 }
