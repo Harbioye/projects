97226a7fe57e74a31c8a0673b1b2d108a4632633
Harbioye <marwatmohammed@yahoo.com>
2017-02-17 00:57:53 -0500

    improved and detailed

--- shell2.c
+++ shell2.c
@@ -1,3 +1,10 @@
+
+/*
+Write a small program that loops reading a line from standard input and checks the first word of the input line. 
+If the first word is one of the following internal commands (or aliases) perform the designated task. 
+Otherwise use the standard ANSI C system function to execute the line through the default system shell.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
@@ -8,11 +15,20 @@
 */   
 extern char **environ;
 
+
+//global variable depicting the number of functions 
+const int NUM_FUNCS = 4;
+int n = 0;
+
 // -- FUNCTION PROTOTYPES --
 void clear_screen();
 void list_directory();
 void list_environment();
 void quit_program();
+
+//arrays are made to point at the respective void functions
+void (*functionArray[NUM_FUNCS])() = {&clear_screen, &list_directory, &list_environment, &quit_program};
+
 // -- ENDS --
 
 
@@ -23,38 +39,49 @@ void clear_screen()
    system("clear");
 }
 
-/*list the current directory contents (ls -al <directory>) - 
-   you will need to provide some command line parsing capability to extract the target directory for listing . 
-   Once you have built the replacement command line, use the system function to execute it
+/* A directory is a storage for organized files with detailed information on both the object names and their attributes
+   list the current directory contents (ls -al <directory>) - provide some command line parsing capability to extract the target directory for listing . 
+   use the system function to execute it
 */
-void list_directory()             
+void list_directory(char **tokenline,int numTokens)             
 {
-   // Note (David.Cooper@cheyney.edu): you declare myStringCopy here, but never initialize it...
-   char myStringCopy[100];
-   // Note (David.Cooper@cheyney.edu): you declare tokenLine here, but never initialize it...
-   char *tokenline[100];
-   // Note (David.Cooper@cheyney.edu): I don't think this does what you want, you may want
-   // to look into how strcat works.
-   strcpy(myStringCopy, "ls -al tokenline[1]");      //copy from directory to copy
-   system(myStringCopy);                 //print out copy
+   //create an array of 500 characters 
+   char direct[500];
+   
+   //copy String ls -al into array direct
+   strcpy(direct, "ls -al ");
+   
+   //check if number of token 
+   if(numTokens > 1)
+   {
+      //get the second string from tokenline[] then concatenate
+      strcat(direct, tokenline[1]);
+   }
+   
+   //call system on direct    
+   system(direct);                 
+
 }
 
-/*list all the environment strings -- 
+/*environment stores information about the terminal type, current locale, user’s home directory, name of curent file and more.
+  list all the environment strings -- 
   environ is an array of pointers to the 
   environment strings terminated with a NULL pointer in the while loop created.
+  NULL pointer represents the end of environment
 */
 void list_environment()
 {
-   int n = 0;
-   
    while(environ[n]!= NULL)
    {
       printf("%s\n", environ[n]);
+      
+      //increment n after each print until the environment is exhausted
       n++;
    }
 }
 
-/*quit from the program with a zero return value. 
+/* quit puts the program to an end. 
+   quit the program with a zero return value. 
    Use the standard exit function.
 */
 void quit_program()
@@ -62,77 +89,109 @@ void quit_program()
    exit(0);
 }
  
- 
+
 //main function
 int main()
 {
    //declare variables
    size_t maxLine = 100;
-   char* myString;
-   char delim[] = " \n"; 
-   char* breakline;
-   // Note (David.Cooper@cheyney.edu): when do you need to use these tokens?
-   char *tokenline[maxLine];
-   int n;     
+   
+   char* myString;                //used to store user input
+   
+   char delim[] = " \n";         //delimiter is a space and next line -- hence every word to be printed ends when it comes across a space.
+
+   char* token;                  //a pointer used to store each tokenized string
+
+   char *cpy_token[maxLine];     //copy each token into an array of maxLine character pointer for further use
+
    int m;
    
-   //actual initialization
-   n = 0;
 
- /*notice the prototype
-   arrays are made to point at the respective void functions
-  */
-   void (*functionArray[4])() = {&clear_screen, &list_directory, &list_environment, &quit_program};
-
-
-   /*make a list of all aliases/internal controls -- declare and initailize array
+    /*make a list of all aliases/internal controls -- declare and initailize array
      A string array is an array of characters and the use of const char tells 
      the compiler that there is no intention to change the data in aliases.
    */
    const char *aliases[]= {"clr","dir","environ","quit"};
+      
+   //an array of integers used to determine when to call the array of void functions with or without arguments 
+   int needargs[] = {0,1,0,0};
+   
+   //This is used to keep track of of the comparison between the tokenized user input and aliases array 
+   int foundMatch = 0;      
    
    /* create a while loop
-      if a character is not read by getline, then -1 will be returned
    */  
    while(1)
    {      
-      printf("Enter a command line argument\n");
+      //prompt for user input
+      printf(">>");
          
-      //read the operator input/line 
+      //read the operator input/line using stdin
       getline (&myString, &maxLine, stdin);
-            
+     
       /*tokenize keyboard input/line using strtok using a for loop
-         delimiter is a space -- hence every word to be printed ends when it comes across a space.
      */
-      // Note (David.Cooper@cheyney.edu): did you come up with this loop yourself? If not, please 
-      // cite your source.
-      for(breakline = strtok(myString, " "); breakline != NULL; breakline = strtok(NULL, delim))
-      { 
-         // Note (David.Cooper@cheyney.edu): TO DEBUG try printing the value of n here:
+      token = strtok(myString,delim);  
+     
+      /*use a while loop if the above token is not empty
+      */
+      while(token != NULL)
+      {
+         //store each token in cpy_token[]
+         cpy_token[n] = strdup(token);
          
-         tokenline[n] = malloc(strlen(breakline)+1);        //allocate some space for new array
-         
-         strcpy(tokenline[n],breakline);
-               
-      /*Next is the comparism of the arrays aliases and tokenline 
-            by calling the functions above
+         //check inbetween each token to ensure that delimiters are assigned \0 and next string is considered if any
+         token = strtok(NULL, delim);                
+
+         n++;
+      }  
+
+      /*Next is the comparison of the arrays aliases and tokenline 
+            loop through the array elements for comparison using a for loop
        */
-         for(m = 0; m < 4; m++)    //loop through the array elements for comparison
-         {   
-            if(strcmp(tokenline[n], aliases[m]) == 0)
+      for(m = 0; m < NUM_FUNCS; m++)    
+      {   
+         //compare the first token with every element in the predefined array of aliases
+         if(strcmp(cpy_token[0],aliases[m]) == 0)
+         {
+            /*confirm if the function to be called requires an argument or not using same index as that 
+               of the aliases found.
+               if the needargs[] == 0, call functionArray without arguments 
+               else call functionArray with arguments.
+            */
+            if(needargs[m] == 0)
             {
                functionArray[m]();
-            }           
-            /* Use standard ANSI C system function to execute the line through the default system shell.
-            */
+            }
             else
             {
-               system(myString);
-            } 
-         }         
-         /* Deallocate allocated memory */
-         free(tokenline[n]);   
+               functionArray[m](cpy_token,n);
+            }
+            
+            //if compared elements are found, then foundmatch is reassigned as 1
+            foundMatch = 1;
+         }
       }
-   }      
+         
+      /* where the first element in cpy_token[0] is not same as any of the elemnts in aliases array,
+         call system function on the entire line of user input---------
+         Use standard ANSI C system function to execute the line through the default system shell.
+      */
+      if(!foundMatch)
+      {
+         system(myString);
+      }       
+      
+      /* Deallocate each allocated memory used as extra storage for the tokenized strings
+      */
+      while(n > 0)
+      {
+         n--;
+         free(cpy_token[n]);   
+      }
+   }
+   //deallocate the entire cpy_token array of maxLine character pointer allocated to hold an extra copy of the tokenized word.
+   free(cpy_token);
+        
    return(0);
 }
